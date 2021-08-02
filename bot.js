55eded87fa66047e606f52e37c9b6aac324529c5
nguyen-charlie00 <nguyen.charlie00@gmail.com>
2021-08-01 22:59:29 -0700

    charlie did everything

--- bot.js
+++ bot.js
@@ -1,1 +1,17 @@
-const Discord = requires('discord.js');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config();
+client.on('ready', () => {
+    console.log("n/a");
+});
+  
+client.on('message', msg => {
+    if (msg.content === 'pencil') {
+      msg.reply('sharpener!');
+    }
+});
+  
+client.login(process.env.BOT_PRIVATE);
+
+//console.log("I am commenting my thingy: ",process.env.BOT_PRIVATE);
+
