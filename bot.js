80ba1666e8af97e6fa09c4d440fc72da217b6ddb
Justin Yu <80501885+justin123yu@users.noreply.github.com>
2021-08-05 19:55:13 -0700

    Added a /help command

--- bot.js
+++ bot.js
@@ -1,16 +1,31 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 require('dotenv').config();
+const { MessageEmbed } = require('discord.js');
+
+
 client.on('ready', () => {
-    console.log("n/a");
+    console.log("Online");
 });
   
-client.on('message', msg => {
-    if (msg.content === 'pencil') {
-      msg.reply('sharpener!');
-    }
+client.on('messageCreate', async message => {
+
+	if (message.content.toLowerCase() === '-+test') {
+		const data =
+    {
+      name:'help',
+      description:'explains how to use bot',
+
+    };
+    console.log(message.content);
+		const command = await client.guilds.cache.get('367198129633886209')?.commands.create(data);
+		console.log(command);
+	}
 });
-  
+
+
+
+
 client.login(process.env.BOT_PRIVATE);
 
 //console.log("I am commenting my thingy: ",process.env.BOT_PRIVATE);
