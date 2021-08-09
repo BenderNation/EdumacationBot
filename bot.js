b37d8b430f31bbaac26470261b8371b5f41eb4d6
Khanh H <Kkhanhhoang@outlook.com>
2021-08-08 19:29:16 -0700

    Added function to list all commands
    Co-authored-by: justin123yu <justin123yu@users.noreply.github.com>

--- bot.js
+++ bot.js
@@ -3,16 +3,16 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 require('dotenv').config();
 const { MessageEmbed } = require('discord.js');
 
-
-client.on('ready', () => {
-    console.log("Online");
+client.once('ready', () => {
+	console.log('Ready!');
 });
-  
+
+
 //Creating Commands to see in the /commands
 client.on('messageCreate', async message => {
 
   if (!client.application?.owner) await client.application?.fetch();
-//Deploy commands to be registered onto Discord
+  //Deploy commands to be registered onto Discord
 	if (message.content.toLowerCase() === '-+deploy' && await client.application?.owner.members.has(message.author.id)) {
 		const data =
     {
@@ -23,21 +23,45 @@ client.on('messageCreate', async message => {
 		console.log(command);
 	}
 });
+
 //First Command Help
 async function runHelp(interaction){
-  await interaction.reply({content:'help page', ephemeral: true});
+
+  const exampleEmbed = new MessageEmbed()
+  .setColor('#0dbadc')
+  .setTitle('Help Page')
+  .setDescription('The List of Commands')
+  .setTimestamp();
+  await interaction.reply({embeds: [exampleEmbed], ephemeral: true});
 };
+
+async function listOfCommands(){
+  var commandList = await client.guilds.cache.get('367198129633886209')?.commands.fetch()
+    .then(commands => commands.map(
+      cmd => {
+        var name = "/"+ cmd['name'];
+        var description = cmd['description'];
+        var options = cmd["options"];
+        return {'Command Name': name, 'Description': description, 'Options': options};
+
+      })
+    );
+  
+  return commandList;
+
+};
+
 
 
 //Utilizing the command
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
     if(interaction.commandName === 'help')
-      runHelp(interaction);
-      await client.guilds.cache.get('367198129633886209')?.commands.fetch()
-      .then(commands => commands.each(
-        cmd => console.log(`Name: ${cmd['name']}\nDescription: ${cmd['description']}`)
-      )).catch(console.error);
+      // runHelp(interaction);
+
+      var test = await listOfCommands();
+      console.log(test);
+
 });
 
 
